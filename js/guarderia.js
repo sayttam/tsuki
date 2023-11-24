edcd1db51dff55e35ac55a0ae68ca0cfdcc963bc
joel.bustos <joel.bustos@gmail.com>
2023-11-23 23:03:13 -0300

    Se corrigio error en Guarderia y se agrando el body del mismo

--- js/guarderia.js
+++ js/guarderia.js
@@ -3,26 +3,23 @@ let dias
 
 function mostrarPresupuestoDetallado(estadia, costoTotal) {
     let resultadoPresupuesto = document.getElementById("resultadoPresupuesto")
-
     let detallePresupuesto = document.createElement("div")
     detallePresupuesto.innerHTML = "<h3>Presupuesto Detallado</h3>"
     detallePresupuesto.innerHTML += "<p>Costo total de la estadia: $" + costoTotal.toFixed(2) + "</p>"
-
     for (let i = 1; i <= dias; i++) {
         detallePresupuesto.innerHTML += "<p>" + i + "° día. Costo: $" + parseFloat(estadia / dias).toFixed(2) + "</p>"
     }
-
     resultadoPresupuesto.innerHTML = ""
     resultadoPresupuesto.appendChild(detallePresupuesto)
 }
 
 function calcularPresupuesto() {
+    precioPorDia = 2000
+    precioPorDia *= 1
     let banderaNombre = false
     let banderaTamaño = false
     let banderaDias = false
-
     let nombreCanino = document.getElementById('nombreCanino').value
-
     if (nombreCanino === "") {
         const sinNombre = document.querySelector(".sinNombre")
         sinNombre.innerHTML = `<p style="color:red;font-weight:bolder;">Debe ingresar un nombre!</p>`
@@ -33,8 +30,7 @@ function calcularPresupuesto() {
     }
 
     let tamaño = parseInt(document.getElementById('tamaño').value)
-
-    if (tamaño < 1 || tamaño > 3 || isNaN(tamaño)) {
+    if (tamaño < 1 || tamaño > 3) {
     } else {
         if (tamaño === 2) {
             precioPorDia *= 1.25
@@ -45,7 +41,6 @@ function calcularPresupuesto() {
     }
 
     dias = parseInt(document.getElementById('dias').value)
-
     if (dias > 0 && dias < 10) {
         banderaDias = true
     } else if (dias >= 10 && dias < 20) {
@@ -59,7 +54,6 @@ function calcularPresupuesto() {
     if (banderaNombre && banderaTamaño && banderaDias) {
         let cuidadosEspeciales = document.getElementById('cuidadosEspeciales').checked
         let costoAdicionalEspeciales = 0
-
         if (cuidadosEspeciales) {
             let opcionSeleccionada = parseInt(document.getElementById('opcionCuidados').value)
             switch (opcionSeleccionada) {
@@ -73,22 +67,12 @@ function calcularPresupuesto() {
                     costoAdicionalEspeciales = 1
                     break
             }
-
             precioPorDia *= costoAdicionalEspeciales
         }
-
         function calcularPrecioEstadia(diasEstadia) {
             return precioPorDia * diasEstadia
         }
-
         let estadia = calcularPrecioEstadia(dias)
-
-        function mostrarPrecioDia(precioTotal) {
-            for (let i = 1; i <= dias; i++) {
-            }
-        }
-
-        mostrarPrecioDia(estadia)
         mostrarPresupuestoDetallado(estadia, calcularPrecioEstadia(dias))
     } 
 }
